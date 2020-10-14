6bb355e7dfe7ef96d3106ced8cc7bd7e2fd1c061
JoseBG-spec <61696362+JoseBG-spec@users.noreply.github.com>
2020-10-13 19:31:28 -0500

    Dos micros a medias

--- RevenantOS.py
+++ RevenantOS.py
@@ -20,11 +20,11 @@ def asignarValoresProceso():
        #print(len(proceso))
        #print(len(TE))
 
-def asignarTCC(index,):
+def asignarTCC(index,vacio):
     global TCC,TF,TI,tiempoCambio,procesoMs
 
     #print("TF",TF)
-    if(index==0):
+    if(vacio==True):
         TCC[index]=0
 
     elif(index>0):
@@ -39,16 +39,16 @@ def asignarTCC(index,):
 def asignarTiempoBloqueo(index):
     global TE,TB,tiempoBloqueo
 
-    TB=[20,20,30,40,20,50,20,30,20,20,50,20,30,20,20,40,30]
+    #TB=[20,20,30,40,20,50,20,30,20,20,50,20,30,20,20,40,30]
 
-    '''if 1<=TE[index]<=400:
+    if 1<=TE[index]<=400:
         TB[index]=2*tiempoBloqueo
     elif 401<=TE[index]<=600:
         TB[index]=3*tiempoBloqueo
     elif 601<=TE[index]<=800:
         TB[index]=4*tiempoBloqueo
     elif 801<=TE[index]<=10000:
-        TB[index]=5*tiempoBloqueo'''
+        TB[index]=5*tiempoBloqueo
 
 def asignarTVC(index):
     global TVC,TE,tiempoQuantum,tiempoBloqueo
@@ -65,16 +65,69 @@ def asignarTT(index):
     global TCC,TE,TVC,TB,TT
     TT[index]=TCC[index]+TE[index]+TVC[index]+TB[index]
 
-def asignarTI(index):
-    global TI,TF
-    if(index==0):
-        TI[0]=0
+def asignarTI(index,vacio,micro,tiempoInicial):
+    global TI,TF,microProcesadores
+    if(tiempoInicial!=0):
+        TI[index]=tiempoInicial
+    if(vacio==True):
+        TI[index]=0
     elif index>0:
-        TI[index]=TF[index-1]
+        TI[index]=microProcesadores[micro][len(microProcesadores[micro])-1].get("TF")
+        #TI[index]=TF[index-1]
 
 def asignarTF(index):
     global TF,TI,TT
     TF[index]=TI[index]+TT[index]
+
+def asignarMicro(index):
+    global microProcesadores,procesoMs
+    micro=0;
+    valoresFinales=[]
+    for x in microProcesadores:
+        if not x:
+            micro=microProcesadores.index(x)
+            return micro, True,0
+            break
+        else:
+            valoresFinales.append(x[len(x)-1].get("TF"))
+    micro=valoresFinales.index(min(valoresFinales))
+    tiempoInicial=0
+    print(valoresFinales)
+    print(min(valoresFinales))
+    if(min(valoresFinales)<procesoMs[index]):
+        tiempoInicial=procesoMs[index]
+
+    return micro, False,tiempoInicial
+
+def asignarPorMicro():
+    procesos,TCC,TE,TVC,TB,TT,TI,TF=[],[],[],[],[],[],[],[]
+    dicccccc=[]
+    for micros in microProcesadores:
+        for x in micros:
+             procesos.append(x.get("Proceso"))
+             TCC.append(x.get("TCC"))
+             TE.append(x.get("TE"))
+             TVC.append(x.get("TVC"))
+             TB.append(x.get("TB"))
+             TT.append(x.get("TT"))
+             TI.append(x.get("TI"))
+             TF.append(x.get("TF"))
+        dict = {"Proceso": procesos,
+       "TCC": TCC,
+       "TE": TE,
+       "TVC": TVC,
+        "TB": TB,
+        "TT": TT,
+        "TI": TI,
+        "TF": TF}
+        print(dict)
+        dicccccc.append(dict)
+
+
+    return dicccccc
+
+
+
 
 
 from tkinter import *
@@ -113,7 +166,8 @@ root = Tk()
 t = Table(root)
 root.mainloop()'''
 
-
+print("¿Cuantos microrocesadores son?:")
+noProcesadores=int(input())
 print("Tiempo duración de cada Quantum:")
 tiempoQuantum=int(input())
 print("Tiempo de bloqueo:")
@@ -121,32 +175,44 @@ tiempoBloqueo=int(input())
 print("Tiempo de cambio:")
 tiempoCambio=int(input())
 
+microProcesadores=[[] for y in range(noProcesadores)]
+
 index=0
 asignarValoresProceso()
 for proc in proceso:
-    asignarTCC(index)
-    asignarTI(index)
+
+    microAsignado,vacio,tiempoInicial = asignarMicro(index)
+    print(microAsignado,vacio)
+    asignarTCC(index,vacio)
+    asignarTI(index,vacio,microAsignado,tiempoInicial)
     asignarTVC(index)
     asignarTiempoBloqueo(index)
     asignarTT(index)
     asignarTF(index)
 
+    dictPerProcess = {"Proceso": proceso[index],
+       "TCC": TCC[index],
+       "TE": TE[index],
+       "TVC": TVC[index],
+        "TB": TB[index],
+        "TT": TT[index],
+        "TI": TI[index],
+        "TF": TF[index]
+                      }
+
+    microProcesadores[microAsignado].append(dictPerProcess)
     #print("TCC",index,TCC)
     index+=1
 
+actualDic=asignarPorMicro()
 
+brics=[]
+for p in actualDic:
+    brics.append(pd.DataFrame(p))
 
-
-dict = {"Proceso": proceso,
-       "TCC": TCC,
-       "TE": TE,
-       "TVC": TVC,
-        "TB": TB,
-        "TT": TT,
-        "TI": TI,
-        "TF": TF}
-brics = pd.DataFrame(dict)
-print(brics)
+for i in brics:
+    print(i)
+    print("------------------------------------------------------------------------------------------")
 #print("----------------")
 #print(TCC)
 
