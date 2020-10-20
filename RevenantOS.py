a80ce8383c40c6a1299f59c84e929efff5ee1cfb
JoseBG-spec <61696362+JoseBG-spec@users.noreply.github.com>
2020-10-20 00:10:25 -0500

    GUI basica
    :O

--- RevenantOS.py
+++ RevenantOS.py
@@ -1,17 +1,3 @@
-noProcesadores=0
-proceso=[0 for _ in range(17)]
-procesoMs=[0 for _ in range(17)]
-TCC= [0 for _ in range(17)]
-TE=[0 for _ in range(17)]
-TVC=[0 for _ in range(17)]
-TB=[0 for _ in range(17)]
-TT=[0 for _ in range(17)]
-TI=[0 for _ in range(17)]
-TF=[0 for _ in range(17)]
-tiempoQuantum=0
-tiempoBloqueo=0
-tiempoCambio=0
-
 def asignarValoresProceso():
        global proceso,TE,procesoMs
        proceso = ["B", "D", "F", "H", "J","L","N","O","A","C","E","G","I","K","M","P","Ñ"]
@@ -144,94 +130,199 @@ def asignarPorMicro():
 
     return dicccccc
 
-
-
+def asignarPorMicroTkinter():
+    inx=1
+    tempDic=[[] for _ in range(18)]
+    for micros in microProcesadores:
+        tempDic[0]=["Proceso","TCC","TE","TVC","TB","TT","TI","TF"]
+        for x in micros:
+            for y in x:
+                tempDic[inx].append(x.get(y))
+            inx+=1
+        tempDic2 = [x for x in tempDic if x != []]
+        print(tempDic2)
+        dicGUI.append(tempDic2)
+        tempDic=[[] for _ in range(18)]
+        inx=1
 
 
 from tkinter import *
+from tkinter import ttk
+from tkinter import messagebox
 import pandas as pd
 import math as math
 
 
-'''class Table:
+proceso=[0 for _ in range(17)]
+procesoMs=[0 for _ in range(17)]
+TCC= [0 for _ in range(17)]
+TE=[0 for _ in range(17)]
+TVC=[0 for _ in range(17)]
+TB=[0 for _ in range(17)]
+TT=[0 for _ in range(17)]
+TI=[0 for _ in range(17)]
+TF=[0 for _ in range(17)]
+noProcesadores=0
+tiempoQuantum=0
+tiempoBloqueo=0
+tiempoCambio=0
+dicGUI=[]
 
-    def __init__(self,root):
 
-        # code for creating table
-        for i in range(total_rows):
-            for j in range(total_columns):
-
-                self.e = Entry(root, width=20, fg='blue',
-                               font=('Arial',16,'bold'))
-
-                self.e.grid(row=i, column=j)
-                self.e.insert(END, lst[i][j])
-
-# take the data
-lst = [(1,'Raj','Mumbai',19),
-       (2,'Aaryan','Pune',18),
-       (3,'Vaishnavi','Mumbai',20),
-       (4,'Rachna','Mumbai',21),
-       (5,'Shubham','Delhi',21)]
-
-# find total number of rows and
-# columns in list
-total_rows = len(lst)
-total_columns = len(lst[0])
-
-# create root window
-root = Tk()
-t = Table(root)
-root.mainloop()'''
-
-print("¿Cuantos microrocesadores son?:")
+'''print("¿Cuantos microrocesadores son?:")
 noProcesadores=int(input())
 print("Tiempo duración de cada Quantum:")
 tiempoQuantum=int(input())
 print("Tiempo de bloqueo:")
 tiempoBloqueo=int(input())
 print("Tiempo de cambio:")
-tiempoCambio=int(input())
+tiempoCambio=int(input())'''
+microProcesadores=[]
+def iniciarProceso():
+    global microProcesadores
+    microProcesadores=[[] for y in range(noProcesadores)]
+    index=0
+    asignarValoresProceso()
+    for proc in proceso:
 
-microProcesadores=[[] for y in range(noProcesadores)]
+        microAsignado,vacio,tiempoInicial = asignarMicro(index)
+        #print(microAsignado,vacio)
+        asignarTCC(index,vacio,microAsignado,tiempoInicial)
+        #print(proceso[index])
+        asignarTI(index,vacio,microAsignado,tiempoInicial)
+        asignarTVC(index)
+        asignarTiempoBloqueo(index)
+        asignarTT(index)
+        asignarTF(index)
 
-index=0
-asignarValoresProceso()
-for proc in proceso:
+        dictPerProcess = {"Proceso": proceso[index],
+           "TCC": TCC[index],
+           "TE": TE[index],
+           "TVC": TVC[index],
+            "TB": TB[index],
+            "TT": TT[index],
+            "TI": TI[index],
+            "TF": TF[index]
+                          }
 
-    microAsignado,vacio,tiempoInicial = asignarMicro(index)
-    #print(microAsignado,vacio)
-    asignarTCC(index,vacio,microAsignado,tiempoInicial)
-    #print(proceso[index])
-    asignarTI(index,vacio,microAsignado,tiempoInicial)
-    asignarTVC(index)
-    asignarTiempoBloqueo(index)
-    asignarTT(index)
-    asignarTF(index)
-
-    dictPerProcess = {"Proceso": proceso[index],
-       "TCC": TCC[index],
-       "TE": TE[index],
-       "TVC": TVC[index],
-        "TB": TB[index],
-        "TT": TT[index],
-        "TI": TI[index],
-        "TF": TF[index]
-                      }
-
-    microProcesadores[microAsignado].append(dictPerProcess)
-    #print("TCC",index,TCC)
-    index+=1
-
-actualDic=asignarPorMicro()
-
+        microProcesadores[microAsignado].append(dictPerProcess)
+        #print("TCC",index,TCC)
+        index+=1
+    asignarPorMicroTkinter()
 brics=[]
-for p in actualDic:
-    brics.append(pd.DataFrame(p))
 
-for i in brics:
-    print(i)
-    print("------------------------------------------------------------------------------------------")
+#actualDic=asignarPorMicro()
+#print(actualDic)
+#for p in actualDic:
+    #brics.append(pd.DataFrame(p))
+
+#for i in brics:
+    #print(i)
+    #print("------------------------------------------------------------------------------------------")
 #print("----------------")
 #print(TCC)
+
+
+'''-----------------------------------------------------------------------------------------------------------------------------------------------------------'''
+'''                                                              GUI                                                                                          '''
+class Table:
+
+    def __init__(self,root):
+        x=0
+        columns=0
+        for p in dicGUI:
+            xp=x+1
+            total_rows = len(p)
+            total_columns = len(p[0])
+            for i in range(xp,total_rows+xp):
+                for j in range(total_columns):
+
+                    self.e = Entry(root, width=dimensionX//(total_columns*12), fg='black', font=('URW Gothic L',14,'bold'))
+
+                    if i!=xp:
+                        self.e.grid(row=i, column=j)
+                        x=i
+                    else:
+                        self.e.grid(row=i, column=j,padx=0,pady=10)
+                        #print("yay")
+
+                    self.e.insert(END, p[i-xp][j])
+        print(x)
+
+
+def createTable():
+    iniciarProceso()
+    w= Frame(tab2)
+    w.pack(side=LEFT,fill=Y)
+    t=(Table(w))
+
+
+
+def checarDatos():
+    global noProcesadores,tiempoQuantum,tiempoBloqueo,tiempoCambio
+    if entry1.get().isnumeric()==True and entry2.get().isnumeric()==True and entry3.get().isnumeric()==True and entry4.get().isnumeric()==True:
+        noProcesadores=int(entry1.get())
+        tiempoQuantum=int(entry2.get())
+        tiempoBloqueo=int(entry3.get())
+        tiempoCambio=int(entry4.get())
+        createTable()
+
+    else:
+        messagebox.showinfo('Advertencia','Favor de llenar todos los campos con datos númericos')
+    entry1.delete(0, 'end')
+    entry2.delete(0, 'end')
+    entry3.delete(0, 'end')
+    entry4.delete(0, 'end')
+
+def borrar():
+    w.grid_forget()
+#GUI
+dimensionX,dimensionY=1000,800
+
+root = Tk()
+root.title("Microprocesadores")
+root.geometry('1000x800')
+s = ttk.Style()
+s.configure('TNotebook.Tab', font=('URW Gothic L','20','bold') )
+tab_control = ttk.Notebook(root)
+
+tab1 = ttk.Frame(tab_control)
+tab2 = ttk.Frame(tab_control)
+w= Frame(tab2)
+s=Scrollbar(tab2)
+s.pack(side=RIGHT,fill=Y)
+
+lbl = Label(tab1, text="Ingresa el numero de microprocesadores", font=('URW Gothic L',14,'bold'))
+lbl.pack(side=TOP)
+entry1=Entry(tab1, font=('URW Gothic L',14,'bold'))
+entry1.pack(side=TOP)
+entry1.focus()
+lb2 = Label(tab1, text="Ingresa la duración de cada Quantum", font=('URW Gothic L',14,'bold'))
+lb2.pack(side=TOP)
+entry2=Entry(tab1, font=('URW Gothic L',14,'bold'))
+entry2.pack(side=TOP)
+lb3 = Label(tab1, text="Ingresa el tiempo de bloqueo", font=('URW Gothic L',14,'bold'))
+lb3.pack(side=TOP)
+entry3=Entry(tab1, font=('URW Gothic L',14,'bold'))
+entry3.pack(side=TOP)
+lb4 = Label(tab1, text="Ingresa el tiempo de cambio", font=('URW Gothic L',14,'bold'))
+lb4.pack(side=TOP)
+entry4=Entry(tab1, font=('URW Gothic L',14,'bold'))
+entry4.pack(side=TOP)
+btn = Button(tab1, text="Crear Tabla",font=('URW Gothic L',14,'bold'),command=checarDatos)
+btn.pack(side=TOP)
+btn2 = Button(tab1, text="Borrar Tabla",font=('URW Gothic L',14,'bold'),command=borrar)
+btn2.pack(side=TOP)
+
+tab_control.add(tab1, text='Datos Generales')
+tab_control.add(tab2, text='Tabla(s)')
+
+tab_control.pack(expand=1, fill='both')
+
+
+
+#s.pack(side=RIGHT,fill= Y)
+
+
+root.mainloop()
 
